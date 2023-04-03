c6988e8511ce1fc139df1413e7d9f582267275d6
Maragathalakshmi <lsk172003@gmail.com>
2023-04-03 11:43:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 function App() {
     return (
-        <div className="App">
-            <header className="App-header" style={{backgroundColor:"olivegreen"}}>
+        <div className="App" >
+            <header className="App-header" style={{ backgroundColor: "pink" }}>
                 <h1 style={{ color: "black", fontFamily: "cursive" }} >Let we see the output of JAVASCRIPT</h1>
+                <br/>
                 <button onClick={window['alertMsg']}  style={{color:"black",fontFamily:"cursive"}}>Click here</button>
             </header>
         </div>
--- src/comp/list.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-
-// function User(props) {
-//     return ( <h1>hii</h1>
-       
-//     );
-// }
-// export default User
